3efe7726f230ad5c06969e5010a7cc5b9594661c
Eros Souza Louback <eroslouback@gmail.com>
2020-10-07 18:26:36 -0300

    Interface desenhando mapa

--- file_processing.py
+++ file_processing.py
@@ -1,50 +1,23 @@
 import numpy as np
-# LINK
-# 
-#
-#
-#
-#
-#
-#
+
 GRASS = 10 
 DESERT = 20
 FLOREST = 100
 MOUNTAIN = 150
 WATER = 180
-"""LINK 6
-SWORD 7
-RED 8
-EMERALD 9
-BLUE 0
-"""
-hyrule = np.loadtxt("hyrule.txt", dtype='int', skiprows=1)
-hyrule2 = [['+' for j in range(42)] for i in range(42)]
-i =j = 0
-for row in hyrule:
-    for col in row:
-        if col==1:
-            hyrule2[i][j]='G'
-        elif col==2:
-            hyrule2[i][j] = 'D'
-        elif col==3:
-            hyrule2[i][j] = 'F'
-        elif col==4:
-            hyrule2[i][j] = 'M'
-        elif col==5:
-            hyrule2[i][j] = 'W'
-        elif col==6:
-            hyrule2[i][j] = 'L'
-        elif col==7:
-            hyrule2[i][j] = 'S'
-        elif col==8:
-            hyrule2[i][j] = 'R'
-        elif col==9:
-            hyrule2[i][j] = 'E'
-        else:
-            hyrule2[i][j] = 'B'
-        j++1
-    i=+1
 
-np.savetxt("hyrule2.txt",hyrule2, fmt='%-0.1s', delimiter=' ', header='Grama=G,Areia=D,Floresta=F,Montanha=M,Agua=A,Link=L,Espada=S,Três pingentes=REB')
-print(input)
+def color_map(fname):
+    hyrule = np.loadtxt(fname, dtype= str,skiprows = 1)
+    return hyrule
+
+#    hyrulenew = np.empty_like(hyrule)
+#    hyrulenew[hyrule == 'G'] = 'D'
+#    hyrulenew[hyrule == 'D'] = 'D'
+#    hyrulenew[hyrule == 'F'] = 'F'
+#    hyrulenew[hyrule == 'M'] = 'M'
+#    hyrulenew[hyrule == 'W'] = 'W'
+#    hyrulenew[hyrule == 'L'] = 'L'
+#    hyrulenew[hyrule == 'S'] = 'S'
+#    hyrulenew[hyrule == 'R'] = 'R'
+#    hyrulenew[hyrule == 'E'] = 'E'
+#    hyrulenew[hyrule == 'B'] = 'B'
--- zelda_maze_gui.py
+++ zelda_maze_gui.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 
+import file_processing as file
+
 sg.theme('DarkAmber')   # Add a touch of color
 
 GRASS = 10 
@@ -8,25 +10,61 @@ FLOREST = 100
 MOUNTAIN = 150
 WATER = 180
 
-# All the stuff inside your window.
-hyrule = [[1 for j in range(42)] for i in range(42)]
-layout =  [[sg.Button('?', size=(1, 1), key=(i,j), pad=(0,0)) for j in range(42)] for i in range(42)]
+RES = 16
+def render_square(graph,t,x,y):
+    square = []
+    GRASS_C =  '#92D050'
+    DESERT_C = '#DDD9C3'
+    FLOREST_C = '#00B050'
+    MOUNTAIN_C = '#948A54'
+    WATER_C = '#0070C0'
 
-def create_map(self):
-    map_layout =[]
-    for i in range(42):
+    if t == 'G':
+        square = graph.DrawRectangle((x*RES,y*RES), (x*RES +RES,y*RES +RES), fill_color=GRASS_C,line_width = 0 )
+    elif t == 'D':
+        square = graph.DrawRectangle((x*RES,y*RES), (x*RES +RES,y*RES +RES), fill_color=DESERT_C,line_width = 0 )
+    elif t == 'F':
+        square = graph.DrawRectangle((x*RES,y*RES), (x*RES +RES,y*RES +RES), fill_color=FLOREST_C,line_width = 0  )
+    elif t == 'M':
+        square = graph.DrawRectangle((x*RES,y*RES), (x*RES +RES,y*RES +RES), fill_color=MOUNTAIN_C,line_width = 0  )
+    elif t == 'W':
+        square = graph.DrawRectangle((x*RES,y*RES), (x*RES +RES,y*RES +RES), fill_color=WATER_C,line_width = 0  )
+    
+
+
+def initial_map(world_map,graph):
+    #makes the basic map shown on screen
+    map_layout = []
+
+    for i in range(len(world_map)):
         row = []
-        for j in range(42):
+        for j in range(len(world_map[i])):
+            row.append(render_square(graph,world_map[i][j],i,j))
+        map_layout.append(row)
+    return map_layout,graph
 
 
+#def build_main_layout:
+world_map = file.color_map("hyrule.txt")
+#layout =  [[sg.Button('?', size=(1, 1), key=(i,j), pad=(0,0)) for j in range(len(world_map))] for i in range(len(world_map))]
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
+graph = sg.Graph(
+                canvas_size=(len(world_map)*RES,len(world_map)*RES),
+                graph_bottom_left=(len(world_map)*RES,0),
+                graph_top_right=(0, len(world_map)*RES),
+                background_color='red',
+                key='graph')
+layout = [
+    [graph]
+]
+
+window = sg.Window('Busca Euristica', layout)
+window.finalize()
+ 
+map_layout,graph = initial_map(world_map,graph)
+
+while True:      
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED:      
         break
-    print('You entered ', values[0])
-
 window.close()
